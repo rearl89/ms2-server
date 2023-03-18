71e8c985eebc8da8635d7a8d351a92ee48f78bff
Roger Earl <rogerearl@me.com>
2023-03-18 10:22:16 -0700

    Created post route. Tested through Postman.

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const cors = require('cors')
 const pool = require('./db')
 
 app.use(cors())
-app.use(express.json)
+app.use(express.json())
 
-app.get('/todos', async (req, res) => {
+//routes
+
+//create
+app.post('/todos', async (req, res) => {
     try {
-       const allTodos = await pool.query('SELECT * FROM todo')
-        res.json(allTodos.rows) 
+        const { description } = req.body
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)", [description])
+        res.json(newTodo)
     } catch (err) {
         console.log(err)
     }
