ba6f7a3993c7547884a7001fdf731a3ec415d13b
Roger Earl <rogerearl@me.com>
2023-03-19 17:30:43 -0700

    Added PUT and DELETE routes

--- index.js
+++ index.js
@@ -41,6 +41,30 @@ app.post('/todos', async (req, res) => {
     
 })
 
+//update
+app.put("/todos/:id", async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const { description } = req.body
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+        res.json('Todo was updated')
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//delete
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+        res.json("Todo was deleted")
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
 app.listen(5001, () => {
     console.log("server has started on port 5001")
 })
